1b76b13316f74293679d171415e878fdb7296a0f
PQuod <63287307+PQuod@users.noreply.github.com>
2020-07-03 16:09:38 +0200

    Update and rename default.html to default.php

--- _layouts/default.php
+++ _layouts/default.php
@@ -2,8 +2,46 @@
 <html lang="{{ page.lang | default: site.lang | default: 'fr' }}">
 
   {%- include head.html -%}
+<?php
+$count = 20;
+function fetch_rand()
+{
+    $images = glob('imgs/*.*');
+    $rand = array_rand($images);
+    return $images[$rand];
+}
 
-  <body>
+$my_list = array();
+$i = 0;
+while($i<$count)
+{
+    $select = fetch_rand();
+    if(!in_array($select,$my_list))
+    {
+        array_push($my_list,$select);
+        $i++;
+    }
+}
+
+//print_r($my_list); //just for check
+
+foreach($my_list as $image)
+{
+    $image_link = explode('.',end(explode('/',$image)));
+    $image_link = $image_link[0];
+    echo $image_link;
+    ?>
+
+
+  <body style="background-image:url(<?php echo $image; ?>)">
+      <div class="tile">
+    <a class="tile-inner" href="imgs/<?php echo $image_link; ?>">
+        <img class="item" src="<?php echo $image; ?>" />
+    </a>
+    </div>
+    <?php
+}
+?>
 	<input class="ouverture-sac-checkbox sr-only" id="ouverture-sac" type="checkbox">
 	<div class="theme-container grow">
 	{%- include header.html -%}
